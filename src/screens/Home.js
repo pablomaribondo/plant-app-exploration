68c253d138d1cdf6e4a0a1ac6b99cdd5795b1621
Pablo Maribondo <pablomgvieira@gmail.com>
2021-03-07 19:14:40 -0300

    Carrossel na Home implementado

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,106 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import {COLORS, FONTS, icons, images, SIZES} from '../constants';
 
 const Home = () => {
+  const [newPlants, setNewPlants] = useState([
+    {id: 0, name: 'Plant 1', img: images.plant1, favourite: false},
+    {id: 1, name: 'Plant 2', img: images.plant2, favourite: false},
+    {id: 2, name: 'Plant 3', img: images.plant3, favourite: false},
+    {id: 3, name: 'Plant 4', img: images.plant4, favourite: false},
+  ]);
+
+  const renderNewPlants = (item, index) => {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginHorizontal: SIZES.base,
+        }}>
+        <Image
+          source={item.img}
+          resizeMode="cover"
+          style={{
+            width: SIZES.width * 0.23,
+            height: '82%',
+            borderRadius: 15,
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={{height: '30%', backgroundColor: COLORS.white}}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomLeftRadius: 50,
+            borderBottomRightRadius: 50,
+            backgroundColor: COLORS.primary,
+          }}>
+          <View
+            style={{
+              marginTop: SIZES.padding * 2,
+              marginHorizontal: SIZES.padding,
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={{color: COLORS.white, ...FONTS.h2}}>New Plants</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  console.log('uiu');
+                }}>
+                <Image
+                  source={icons.focus}
+                  resizeMode="contain"
+                  style={{width: 20, height: 20}}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View style={{marginTop: SIZES.base}}>
+              <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={newPlants}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item, index}) => renderNewPlants(item, index)}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={{height: '50%', backgroundColor: COLORS.lightGray}}>
+        <View />
+      </View>
+
+      <View style={{height: '20%', backgroundColor: COLORS.lightGray}}>
+        <View />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Home;
